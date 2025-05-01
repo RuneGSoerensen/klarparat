f0253034b15bf7f0342458684890dcafbc65d588
RuneGSoerensen <rgs@bricksite.com>
2025-05-01 23:59:28 +0200

    Potential deployment fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  webpack: (config) => {
+    // Exclude the functions directory from the build
+    config.externals = [...(config.externals || []), { 'firebase-functions': 'firebase-functions' }];
+    return config;
+  },
 };
 
 export default nextConfig;
